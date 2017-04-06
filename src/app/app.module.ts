bc27ebf6acb881f57fb2e55244f4d77f8a510acc
Saqlain Gardezi <saqlaingardezi86@gmail.com>
2017-04-06 12:42:04 +0500

    Used Multiple components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- ngModel lives here
+
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ // Components, pipes and directives
+    AppComponent,
+    HeroDetailComponent
   ],
-  imports: [
+  imports: [	// only classes that are NGModule
     BrowserModule,
     FormsModule  // <-- import FormsModule before binding with  [(ngModule)]
   ],
-  providers: [],
+  providers: [], // services
   bootstrap: [AppComponent]
 })
 export class AppModule { }
